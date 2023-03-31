52fea2983d01fa8806e396a710a119a326f2b908
TeddyGavi <matc.davis@gmail.com>
2023-03-31 14:05:49 -0700

    working through sliding window

---
+++ algs/JsAlgs&DSClass/maxSubarraySum.ts
@@ -0,0 +1,31 @@
+/**
+ * @description  accepts an array on integers and a number n, the function should calculate the maximum sum of n consecutive elements in the array
+ * @param {number[]} arr array in integers
+ * @param {number} n  the number of consecutive digits
+ * @returns {number} the max sum that can be created by forming a subarray of size n
+ */
+const maxSubarraySum = (arr: number[], n: number): number => {
+  let maxSum = 0;
+  let tempSum = 0;
+
+  // create temp max
+  if (arr.length < n) return null;
+  for (let i = 0; i < n; i++) {
+    maxSum += arr[i];
+  }
+
+  tempSum = maxSum;
+
+  for (let i = n; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - n] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+
+  return maxSum;
+};
+
+console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); //10
+console.log(maxSubarraySum([4, 2, 1, 6], 1)); // 6
+console.log(maxSubarraySum([], 4)); //null;)
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)); // 13
---
+++ notes/patterns/3_sliding-window.md
@@ -0,0 +1,24 @@
+# Sliding Window
+
+- involves creating a window which can either be an array or a number from on position to another
+- depending on condition the window either increases or closes (and a new one is created)
+- useful for keeping track of a subset of data in an array/string
+
+### longestUniqueSubsequence
+
+- "hellothere"
+- "heloth"
+
+### maxSubarraySum
+
+- accepts an array on integers and a number n, the function should calculate the maximum sum of n consecutive elements in the array
+-
+-
+
+```js
+maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3);
+maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2); //10
+maxSubarraySum([4, 2, 1, 6], 1); // 6
+maxSubarraySum([], 4); //null;
+maxSubarraySum([4, 2, 1, 6, 2], 4); // 13
+```
