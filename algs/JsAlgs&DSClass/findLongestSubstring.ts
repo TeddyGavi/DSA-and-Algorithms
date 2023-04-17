3548eff9c18e52329bcb2879617e0cdbc1823f56
TeddyGavi <matc.davis@gmail.com>
2023-04-17 09:59:23 -0700

    sliding window with set

--- algs/JsAlgs&DSClass/findLongestSubstring.ts
+++ algs/JsAlgs&DSClass/findLongestSubstring.ts
@@ -11,5 +11,45 @@ findLongestSubstring('thisishowwedoit') // 6
 Time Complexity - O(n) */
 
 const findLongestSubstring = (str: string): number => {
-  return 1;
+  // let longest = 0;
+  // let seen = {};
+  // let start = 0;
+
+  // for (let i = 0; i < str.length; i++) {
+  //   let char = str[i];
+  //   if (seen[char]) {
+  //     start = Math.max(start, seen[char]);
+  //     console.log(char, i, start);
+  //   }
+  //   // index - beginning of substring + 1 (to include current in count)
+  //   longest = Math.max(longest, i - start + 1);
+  //   // store the index of the next char so as to not double count
+  //   seen[char] = i + 1;
+  //   console.log(seen, longest);
+  // }
+  // return longest;
+
+  let length = 0;
+  let set = new Set<string>();
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    while (set.has(char)) {
+      set.delete(str[start]);
+      start = start + 1;
+    }
+    set.add(char);
+    length = Math.max(length, i - start + 1);
+    // console.log(set);
+  }
+  return length;
 };
+
+console.log(findLongestSubstring("")); // 0
+console.log(findLongestSubstring("rithmschool")); // 7
+console.log(findLongestSubstring("thisisawesome")); // 6
+console.log(findLongestSubstring("thecatinthehat")); // 7
+console.log(findLongestSubstring("bbbbbb")); // 1
+console.log(findLongestSubstring("longestsubstring")); // 8
+console.log(findLongestSubstring("thisishowwedoit")); // 6
