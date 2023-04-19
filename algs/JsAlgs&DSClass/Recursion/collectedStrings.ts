a81f4b7f544d19833cd6dbde72b3e0f8b6a81850
TeddyGavi <matc.davis@gmail.com>
2023-04-19 09:13:48 -0700

    immutable pattern for transforming a nested num to string

---
+++ algs/JsAlgs&DSClass/Recursion/collectedStrings.ts
@@ -0,0 +1,24 @@
+/* collectStrings
+Write a function called collectStrings which accepts an object and returns an array of all the values in the object that have a typeof string
+
+*/
+
+const obj = {
+  stuff: "foo",
+  data: {
+    val: {
+      thing: {
+        info: "bar",
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: "baz",
+          },
+        },
+      },
+    },
+  },
+};
+
+const collectStrings = (obj: Object): string[] => {};
+
+collectStrings(obj); // ["foo", "bar", "baz"])
--- algs/JsAlgs&DSClass/Recursion/stringifyNumbers.ts
+++ algs/JsAlgs&DSClass/Recursion/stringifyNumbers.ts
@@ -2,23 +2,23 @@
 Write a function called stringifyNumbers which takes in an object and finds all of the values which are numbers and converts them to strings. Recursion would be a great way to solve this!
  */
 
-/*
-let obj = {
-    num: 1,
-    test: [],
-    data: {
-        val: 4,
-        info: {
-            isRight: true,
-            random: 66
-        }
-    }
-}
-/*
+let obj1 = {
+  num: 1,
+  test: [],
+  data: {
+    val: 4,
+    info: {
+      isRight: true,
+      random: 66,
+    },
+  },
+};
 
+/* 
+RESULT: 
 stringifyNumbers(obj)
 
-/*
+
 {
     num: "1",
     test: [],
@@ -29,7 +29,25 @@ stringifyNumbers(obj)
             random: "66"
         }
     }
-}
-*/
+} */
 
-const stringifyNumbers = (obj: Object): Object => {};
+const stringifyNumbers = (obj: Object): Object => {
+  const copied = JSON.parse(JSON.stringify(obj)); //Object.assign({}, obj);
+  const helper = (obj: Object) => {
+    Object.keys(obj).forEach((key) => {
+      let val = obj[key];
+      if (typeof val === "number") {
+        obj[key] = val.toString();
+      } else if (typeof val === "object" && !Array.isArray(val)) {
+        return helper(val);
+      }
+    });
+  };
+  helper(copied);
+  return copied;
+};
+
+console.log(obj1);
+console.log(stringifyNumbers(obj1));
+
+console.log(obj1);
