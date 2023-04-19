8ec7c16aa6ac216b989b40abb96ce91031758900
TeddyGavi <matc.davis@gmail.com>
2023-04-19 08:44:31 -0700

    capitalize words same as capitalize first...could be solved a better way

--- algs/JsAlgs&DSClass/Recursion/capitalizeWords.ts
+++ algs/JsAlgs&DSClass/Recursion/capitalizeWords.ts
@@ -2,9 +2,23 @@
 Write a recursive function called capitalizeWords. Given an array of words, return a new array containing each word capitalized.
  */
 
-function capitalizeWords() {
-  // add whatever parameters you deem necessary - good luck!
-}
+const capitalizeWords = (arr: string[]): string[] => {
+  const result = [];
+  const helper = (arr: string[]): void => {
+    if (arr.length === 0) return;
+    let word = arr[0];
+    word = word
+      .split("")
+      .map((x) => x.toUpperCase())
+      .join("");
+    result.push(word);
+    return helper(arr.slice(1));
+  };
+  helper(arr);
+  return result;
+};
 
-// let words = ['i', 'am', 'learning', 'recursion'];
+let words = ["i", "am", "learning", "recursion"];
 // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
+
+console.log(capitalizeWords(words));
