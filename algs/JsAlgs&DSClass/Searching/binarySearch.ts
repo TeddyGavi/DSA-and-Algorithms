5e3f7de613054b8bd61010464a2df9512ead1225
TeddyGavi <matc.davis@gmail.com>
2023-04-19 10:12:12 -0700

    searching notes updated with binary search

---
+++ algs/JsAlgs&DSClass/Searching/binarySearch.ts
@@ -0,0 +1,51 @@
+const binarySearch = (arr: number[], value: number): number => {
+  let min = 0;
+  let max = arr.length - 1;
+
+  while (min <= max) {
+    let guess = Math.floor((min + max) / 2);
+    // console.log(guess);
+
+    if (arr[guess] === value) {
+      return guess;
+    } else if (arr[guess] < value) {
+      min = guess + 1;
+    } else if (arr[guess] > value) {
+      max = guess - 1;
+    }
+  }
+
+  return -1;
+};
+
+console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
+console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    10
+  )
+); // 2
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    95
+  )
+); // 16
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    100
+  )
+); // -1
--- notes/06_Searching.md
+++ notes/06_Searching.md
@@ -16,3 +16,22 @@
   - find
   - findIndex
 - but how do they work?
+- see [Linear Search](../algs/JsAlgs%26DSClass/Searching/linearSearch.ts)
+- O(n)
+
+### Binary Search
+
+- IF the data is sorted, much faster O(log n)
+- eliminates half the remaining elements at a time
+- a Divide and Conquer technique
+- pick the middle as a guess!
+
+- rough pseudocode
+  - function accepts a sorted array, and a search value
+  - create two vars, one points to start and one point to end
+  - while the left pointer comes before the right
+    - make a guess!
+    - is the guess === value return index
+    - if the guess < val move start pointer (min up)
+    - if the guess > val move end pointer down
+  - not found, return -1
