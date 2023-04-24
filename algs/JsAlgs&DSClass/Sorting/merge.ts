c0ac7c3f06e24a8752b76a766fe308f3a955e85d
TeddyGavi <matc.davis@gmail.com>
2023-04-24 11:17:52 -0700

    working on recursive merge

--- algs/JsAlgs&DSClass/Sorting/merge.ts
+++ algs/JsAlgs&DSClass/Sorting/merge.ts
@@ -1,12 +1,15 @@
 const arr1 = [1, 10, 50];
 const arr2 = [2, 14, 99, 100];
 
+const arr3 = [1, 2, 4, 6, 9];
+const arr4 = [10, 15, 17];
+
 const merge = (arr1: number[], arr2: number[]): number[] => {
   const results = [];
   let i = 0;
   let j = 0;
 
-  while (i < arr1.length || j < arr2.length) {
+  while (i < arr1.length && j < arr2.length) {
     if (arr1[i] < arr2[j]) {
       results.push(arr1[i]);
       i++;
@@ -16,7 +19,19 @@ const merge = (arr1: number[], arr2: number[]): number[] => {
     }
   }
 
+  // could leave these out if you use an || in the first while loop
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+
   return results;
 };
 
 console.log(merge(arr1, arr2));
+console.log(merge(arr3, arr4));
+console.log(merge([], arr2));
--- notes/08_Intermediate_Sorting.md
+++ notes/08_Intermediate_Sorting.md
@@ -18,12 +18,18 @@
 - How do we merge?
 - given two arrays which are sorted, this helper should create a new array which is also sorted
 - this should run in O(n + m) space, and should not modify the parameters passed into it
+
   - Create an empty array, take a look at the smallest values in each input array
   - j and i start at 0
   - while there are still values we haven't looked at
     - compare the values of the first array to the second array
       - if the value if smaller than push the value into the first array
       - otherwise, push the value of the second array into the results array
+
+- now we have the merge function
+- break the array into two halves with `array.slice(0, half!)`
+- keep going recursively and send each half into merge
+- once the arr.length is 1 then we return th merged array
 
 ### Quick Sort
 
