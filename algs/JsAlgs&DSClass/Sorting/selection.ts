ec0d3db13556b3cd95ba3ee5acdfc014afbd852c
TeddyGavi <matc.davis@gmail.com>
2023-04-26 13:21:01 -0700

    slight refactor of selection

--- algs/JsAlgs&DSClass/Sorting/selection.ts
+++ algs/JsAlgs&DSClass/Sorting/selection.ts
@@ -4,14 +4,11 @@ const arr1 = [29, 12, 10, 14, 30, 37, 18];
 const arr2 = [5, 3, 4, 1, 2];
 
 const selection = (arr: any[]): any[] => {
-  let min;
   let minIdx;
   for (let i = 0; i < arr.length; i++) {
-    min = arr[i];
     minIdx = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < min) {
-        min = arr[j];
+      if (arr[j] < arr[minIdx]) {
         minIdx = j;
       }
     }
