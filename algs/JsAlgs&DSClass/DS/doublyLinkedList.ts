4a061484352bb5e5ceabf1b2b1ada197ca3a20e3
TeddyGavi <matc.davis@gmail.com>
2023-07-03 22:03:00 -0700

    a couple dll methods!

--- algs/JsAlgs&DSClass/DS/doublyLinkedList.ts
+++ algs/JsAlgs&DSClass/DS/doublyLinkedList.ts
@@ -14,9 +14,69 @@ class DLL {
   head: null | NodeDLL;
   tail: null | NodeDLL;
   length: number;
+
   constructor() {
     this.head = null;
     this.tail = null;
     this.length = 0;
   }
+
+  print() {
+    if (this.length === 0) return "Nothing to print";
+    let current = this.head;
+    while (current) {
+      console.log(current.val);
+      current = current.next;
+    }
+  }
+  push(val: number | string) {
+    const node = new NodeDLL(val);
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    const tail = this.tail;
+    if (this.length === 1) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
+
+    this.length--;
+    return tail;
+  }
+
+  shift() {}
+
+  unShift() {}
+
+  get() {}
+
+  set() {}
+
+  insert() {}
+
+  remove() {}
 }
+
+const dll = new DLL();
+dll.push(1);
+dll.push(2);
+dll.push(3);
+dll.push(4);
+dll.push(5);
+dll.print();
+dll.pop();
+dll.print();
--- notes/dataStructures/03_doublyLinkedList.md
+++ notes/dataStructures/03_doublyLinkedList.md
@@ -6,3 +6,6 @@
 - Each node has a pointer to the next node and the previous node
 - This makes a huge difference in some of the methods
 - Take more memory!
+
+- Need to make sure that you keep track of the next and prev of each node
+- push! Add at end
