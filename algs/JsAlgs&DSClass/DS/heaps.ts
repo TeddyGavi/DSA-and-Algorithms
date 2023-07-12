38a7a24a8c40a257b7f9f5868becd8597a86fb76
TeddyGavi <matc.davis@gmail.com>
2023-07-12 09:40:39 -0700

    more heapying

--- algs/JsAlgs&DSClass/DS/heaps.ts
+++ algs/JsAlgs&DSClass/DS/heaps.ts
@@ -1,1 +1,65 @@
 // Easy way is using an array!
+
+class MaxBinaryHeap {
+  values: number[];
+
+  constructor() {
+    this.values = [];
+  }
+
+  private swap(firstIdx: number, secondIdx: number) {
+    [this.values[firstIdx], this.values[secondIdx]] = [
+      this.values[secondIdx],
+      this.values[firstIdx],
+    ];
+  }
+
+  private findParentIdx(index: number): number {
+    return Math.floor((index - 1) / 2);
+  }
+
+  private leftChild(index: number): number {
+    return 2 * index + 1;
+  }
+  private rightChild(index: number): number {
+    return 2 * index + 2;
+  }
+  private bubbleUp() {
+    let index = this.values.length - 1;
+    let parentIdx = this.findParentIdx(index);
+    while (this.values[parentIdx] < this.values[index] && parentIdx >= 0) {
+      this.swap(parentIdx, index);
+      index = parentIdx;
+      parentIdx = this.findParentIdx(index);
+    }
+  }
+
+  insert(val: number) {
+    this.values.push(val);
+    this.values.length === 1 ? this.values : this.bubbleUp();
+  }
+
+  remove(): number {
+    this.swap(0, this.values.length - 1);
+    const oldRoot = this.values.pop();
+
+    return oldRoot;
+  }
+}
+
+// const maxheap = new MaxBinaryHeap();
+// maxheap.insert(100);
+// maxheap.insert(10);
+// maxheap.insert(9);
+// maxheap.insert(1);
+// maxheap.insert(200);
+const heap = new MaxBinaryHeap();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+console.log(heap);
+heap.insert(55);
+console.log(heap);
--- notes/dataStructures/06_heaps.md
+++ notes/dataStructures/06_heaps.md
@@ -17,3 +17,24 @@
 
 - Parent nodes are always smaller than children
 - root is the smallest!
+
+### Insert into Max
+
+- only place for it to go is at the end! but we need to **bubble up!** we swap until it finds it corrects spot
+  steps
+
+1. push the value to insert into the values array
+2. bubble up
+
+- var to hold index (this.values.length -1)
+- create a parentIndex which is floor of (index -1)/2
+- loop as long as the values element at the parent Index is less than the values element at the child index
+  - swap the value of values element at the parent index with the value of the element property at the child index
+  - set the index to be the parent index and start over
+
+### Remove the max!
+
+- aka. Extract max
+- the procedure for deleting the root from the heap, effectively extracting the max element or min element (depending on max/min heap) and restoring the properties is called down-heap
+- bubble-down, percolate-down, sift-down, trickle down, heapify-down, cascade-down
+  swap the last value with the root, then compare the left and right children with the new "root", swap if needed!
