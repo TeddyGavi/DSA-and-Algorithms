b0c8ac624ae5a5ab44c32cf93d3f30f91bcb0f7d
TeddyGavi <matc.davis@gmail.com>
2023-07-21 10:33:37 -0700

    priority max q'ing

--- algs/JsAlgs&DSClass/DS/priorityQueue.ts
+++ algs/JsAlgs&DSClass/DS/priorityQueue.ts
@@ -1,17 +1,17 @@
 class Node {
-  val: number | null;
+  val: number | string | null;
   priority: number | null;
-  constructor(val = null, priority = null) {
+  constructor(val: string | number, priority: number) {
     this.val = val;
     this.priority = priority;
   }
 }
 
 class PriorityQueue {
-  values: Node[] | null;
+  values: Node[];
 
   constructor() {
-    this.values = null;
+    this.values = [];
   }
   /**
    * @abstract swap the entire node!
@@ -22,23 +22,96 @@ class PriorityQueue {
       this.values[idx1],
     ];
   }
-  bubbleUp() {
+
+  /**
+   *
+   * @param {number} index Index of parent
+   * @returns {number} index of the left child
+   */
+
+  private leftChild(index: number): number {
+    return 2 * index + 1;
+  }
+
+  /**
+   * @param {number} index Index of parent
+   * @returns {number} index of right child!
+   */
+
+  private rightChild(index: number): number {
+    return 2 * index + 2;
+  }
+
+  private bubbleUp() {
     let index = this.values.length - 1;
     let parentIdx = Math.floor((index - 1) / 2);
-
-    while (this.values[parentIdx].priority < this.values[index].priority) {
+    console.log(this.values[parentIdx]);
+    while (
+      parentIdx >= 0 &&
+      this.values[parentIdx].priority < this.values[index].priority
+    ) {
       this.swap(parentIdx, index);
       index = parentIdx;
       parentIdx = Math.floor((index - 1) / 2);
     }
   }
 
-  insert(val: number, priority: number) {
+  enqueue(val: number | string, priority: number) {
     const newNode = new Node(val, priority);
     this.values.push(newNode);
-    this.bubbleUp();
+    this.values.length === 1 ? this : this.bubbleUp();
+  }
+
+  dequeue(): Node {
+    if (this.values.length === 0) return null;
+    this.swap(0, this.values.length - 1);
+    const oldRoot = this.values.pop();
+    let parentIdx = 0;
+    if (this.values.length > 0) {
+      const element = this.values[0];
+      const length = this.values.length;
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        const leftChild = this.leftChild(parentIdx);
+        const rightChild = this.rightChild(parentIdx);
+        let swap = null;
+        let left: Node, right: Node;
+        if (leftChild < length) {
+          left = this.values[leftChild];
+          if (left.priority > element.priority) {
+            swap = leftChild;
+          }
+        }
+
+        if (rightChild < length) {
+          right = this.values[rightChild];
+          if (
+            (swap === null && right.priority > element.priority) ||
+            (swap !== null && right.priority > left.priority)
+          ) {
+            swap = rightChild;
+          }
+        }
+
+        if (swap === null) break;
+        this.swap(parentIdx, swap);
+        parentIdx = swap;
+      }
+    }
+
+    return oldRoot;
   }
 }
 
 const q = new PriorityQueue();
+console.log(q);
+q.enqueue("cold", 1);
+q.enqueue("broken arm", 3);
+q.enqueue("concussion", 5);
+q.enqueue("glass ct", 2);
+q.enqueue("table saw injury", 4);
+
+console.log(q);
+q.dequeue();
+q.dequeue();
 console.log(q);
