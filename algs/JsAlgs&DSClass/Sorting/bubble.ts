bd23b1b81e4ebdb8cfcbd6640a537a8921aa1ef8
TeddyGavi <matc.davis@gmail.com>
2023-07-20 21:46:27 -0700

    rewrite bubblin from memory

--- algs/JsAlgs&DSClass/Sorting/bubble.ts
+++ algs/JsAlgs&DSClass/Sorting/bubble.ts
@@ -2,10 +2,10 @@ const arr1 = [29, 12, 10, 14, 30, 37, 18];
 const arr2 = [5, 3, 4, 1, 2];
 
 export const swap = (
-  arr: Array<any>,
+  arr: Array<number | string>,
   idx1: number,
   idx2: number,
-): Array<any> => {
+): Array<number | string> => {
   const temp = arr[idx1];
   arr[idx1] = arr[idx2];
   arr[idx2] = temp;
@@ -25,7 +25,7 @@ export const swap = (
 //   }
 //   return arr;
 // };
-const bubblin = (arr: Array<any>) => {
+const bubblin = (arr: Array<number | string>) => {
   let noSwaps: boolean;
   for (let i = arr.length; i > 0; i--) {
     console.log(arr);
@@ -42,3 +42,18 @@ const bubblin = (arr: Array<any>) => {
 };
 console.log(bubblin(arr1));
 console.log(bubblin(arr2));
+
+const bubblely = (arr: number[]): number[] => {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(bubblely(arr1));
