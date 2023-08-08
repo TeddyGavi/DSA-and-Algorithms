6b19287adea5683e6bf6b423feb69ada1c0fff00
TeddyGavi <matc.davis@gmail.com>
2023-08-08 09:37:04 -0700

    updating hash

--- algs/JsAlgs&DSClass/DS/hash.ts
+++ algs/JsAlgs&DSClass/DS/hash.ts
@@ -18,3 +18,23 @@ console.log(hash("pink", 10));
 console.log(hash("pink", 10));
 
 // only strings!, not constant time, could be more distributed 
+
+function hashTwo(key: string, arrayLength: number) {
+  let total = 0
+  const weird = 31
+
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    const char = key[i]
+    const value = char.charCodeAt(0) - 96
+    total = (total * weird + value) & arrayLength
+  }
+  return total
+}
+
+console.log(hashTwo("red", 10));
+console.log(hashTwo("red", 10));
+console.log(hashTwo("red", 10));
+console.log(hashTwo("red", 10));
+console.log(hashTwo("pink", 10));
+console.log(hashTwo("pink", 10));
+console.log(hashTwo("pink", 10));
